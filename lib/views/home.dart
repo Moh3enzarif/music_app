cf281616c467a99ea31dbcac9b9627fb8e3726ad
moh3n zarif <mohsenzarif70@gmail.com>
2024-10-14 19:08:53 +0330

    created songs

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,61 +1,92 @@
 import 'package:brozerplaylist/consts/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:brozerplaylist/consts/text_style.dart';
+import 'package:brozerplaylist/controller/player_controller.dart';
+import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var controller = Get.put(PlayerController());
+
     return Scaffold(
-      backgroundColor: bgDarkColor,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
-              color: bgColor,
+        backgroundColor: bgDarkColor,
+        appBar: AppBar(
+          backgroundColor: bgDarkColor,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: whiteColor,
+              ),
             ),
+          ],
+          leading: const Icon(
+            Icons.sort_rounded,
+            color: whiteColor,
           ),
-        ],
-        leading: const Icon(
-          Icons.sort_rounded,
-          color: bgColor,
+          title: Text(
+            'BrozerPlayList',
+            style: ourStyle(size: 18),
+          ),
         ),
-        title: Text(
-          'BrozerPlayList',
-          style: ourStyle(size: 18),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, int index) {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ListTile(
-                title: Text(
-                  'Music name',
-                  style: ourStyle(family: bold, size: 15),
+        body: FutureBuilder<List<SongModel>>(
+          future: controller.audioQuery.querySongs(
+            ignoreCase: true,
+            orderType: OrderType.ASC_OR_SMALLER,
+            sortType: null,
+            uriType: UriType.EXTERNAL,
+          ),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.data == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.data!.isEmpty) {
+              return Center(
+                child: Text(
+                  "No song found",
+                  style: ourStyle(),
                 ),
-                subtitle: Text(
-                  'Artist name',
-                  style: ourStyle(family: light, size: 12),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: 100,
+                  itemBuilder: (context, int index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        tileColor: bgColor,
+                        title: Text(
+                          'Music name',
+                          style: ourStyle(family: bold, size: 15),
+                        ),
+                        subtitle: Text(
+                          'Artist name',
+                          style: ourStyle(family: light, size: 12),
+                        ),
+                        trailing: const Icon(
+                          Icons.play_arrow,
+                          color: whiteColor,
+                          size: 26,
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                trailing: const Icon(
-                  Icons.play_arrow,
-                  color: whiteColor,
-                  size: 26,
-                ),
-              ),
-            );
+              );
+            }
           },
-        ),
-      ),
-    );
+        ));
   }
 }
